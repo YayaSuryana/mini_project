9bf79753e85cb457b4f1fbea547a65109bb36298
YayaSuryana <yayasuryana542@gmail.com>
2023-04-21 12:32:08 +0700

    membuat entity kampanye dan kampanye images

--- kampanye/entity.go
+++ kampanye/entity.go
@@ -1,1 +1,27 @@
 package kampanye
+
+import "time"
+
+type Kampanye struct{
+	ID 					int
+	UserID 				int
+	Name				string
+	ShortDescription 	string
+	Description 		string
+	Perks				string
+	BackerCount			int
+	GoalAmount			int
+	CurrentAmount		int
+	Slug				string
+	CreatedAt			time.Time
+	UpdatedAt			time.Time
+}
+
+type KampanyeImage struct{
+	ID 				int
+	KampanyeID		int
+	FileName		string
+	IsPrimary		int
+	CreatedAt		time.Time
+	UpdatedAt		time.Time
+}
